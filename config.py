072a0703c424fd54962cdf0c2994a5c50e758ee3
Soutick <soutickadak99@gmail.com>
2025-03-03 20:26:07 +0530

    Update config.py

--- config.py
+++ config.py
@@ -10,19 +10,19 @@ from logging.handlers import RotatingFileHandler
 
 
 #Bot token @Botfather
-TG_BOT_TOKEN = os.environ.get("TG_BOT_TOKEN", "7968053738:AAErIyHT7vFqo_bpQJoQuAKDrST4A_7Recs")
+TG_BOT_TOKEN = os.environ.get("TG_BOT_TOKEN", "7766320329:AAHwBTedA-7BHLTnDNPQugvHBjXkLxYKZKw")
 
 #Your API ID from my.telegram.org
-APP_ID = int(os.environ.get("APP_ID", "28450765"))
+APP_ID = int(os.environ.get("APP_ID", "27732178"))
 
 #Your API Hash from my.telegram.org
-API_HASH = os.environ.get("API_HASH", "36f00f11f9d5c65e69b81fd804453a93")
+API_HASH = os.environ.get("API_HASH", "6678ce8ac722de41787329bba54ef8d9")
 
 #Your db channel Id
 CHANNEL_ID = int(os.environ.get("CHANNEL_ID", "-1002314807164"))
 
 #OWNER ID
-OWNER_ID = int(os.environ.get("OWNER_ID", "5827289728"))
+OWNER_ID = int(os.environ.get("OWNER_ID", "6718471228"))
 
 #Port
 PORT = os.environ.get("PORT", "8080")
@@ -32,19 +32,19 @@ DB_URI = os.environ.get("DATABASE_URL", "mongodb+srv://FileSout:FileSout@cluster
 DB_NAME = os.environ.get("DATABASE_NAME", "Cluster0")
 
 #force sub channel id, if you want enable force sub
-FORCE_SUB_CHANNEL_1 = int(os.environ.get("FORCE_SUB_CHANNEL_1", "-1002445963619"))
-FORCE_SUB_CHANNEL_2 = int(os.environ.get("FORCE_SUB_CHANNEL_2", "-1002296605015"))
-FORCE_SUB_CHANNEL_3 = int(os.environ.get("FORCE_SUB_CHANNEL_3", "-1002118318196"))
-FORCE_SUB_CHANNEL_4 = int(os.environ.get("FORCE_SUB_CHANNEL_4", "-1002287605942"))
+FORCE_SUB_CHANNEL_1 = int(os.environ.get("FORCE_SUB_CHANNEL_1", "-1001988756880"))
+FORCE_SUB_CHANNEL_2 = int(os.environ.get("FORCE_SUB_CHANNEL_2", "-1002018010845"))
+FORCE_SUB_CHANNEL_3 = int(os.environ.get("FORCE_SUB_CHANNEL_3", "-1002378389862"))
+FORCE_SUB_CHANNEL_4 = int(os.environ.get("FORCE_SUB_CHANNEL_4", "-1002487535307"))
 
 
 TG_BOT_WORKERS = int(os.environ.get("TG_BOT_WORKERS", "4"))
 
 #start message
-START_MSG = os.environ.get("START_MESSAGE", "<b>Hello!! {mention} 👋! I'm Alya.\n\nI'm here to provide you adult contents for Free 😄\n\n© @AIO_Backup</b>")
+START_MSG = os.environ.get("START_MESSAGE", "<b>Hello!! {mention} 👋! I'm here to provide you adult contents for Free 😄\n\n© @AIO_Backup</b>")
 try:
     ADMINS=[5827289728]
-    for x in (os.environ.get("ADMINS", "7272399911").split()):
+    for x in (os.environ.get("ADMINS", "5827289728").split()):
         ADMINS.append(int(x))
 except ValueError:
         raise Exception("Your Admins list does not contain valid integers.")
@@ -64,7 +64,7 @@ DISABLE_CHANNEL_BUTTON = os.environ.get("DISABLE_CHANNEL_BUTTON", None) == 'Fals
 BOT_STATS_TEXT = "<b>BOT UPTIME</b>\n{uptime}"
 USER_REPLY_TEXT = "Good Luck for Your Future!"
 
-ADMINS.append(7272399911)
+ADMINS.append(6718471228)
 ADMINS.append(5827289728)
 
 LOG_FILE_NAME = "filesharingbot.txt"
